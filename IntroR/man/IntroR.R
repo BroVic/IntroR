cc3edcec91f040201008afa6d4779d015e914ba7
Victor Ordu <Victor Ordu>
2016-02-18 23:53:47 +0100

    Documentation for the IntroR package

---
+++ IntroR/man/IntroR.R
@@ -0,0 +1,16 @@
+#' IntroR: A package for introductory R training for Nigeria's public service.
+#'
+#' The IntroR package contains material that will help participants very
+#' quickly get started with the R Programming Language. The main aim of the
+#' approach employed is to enable the new R user to grasp basic concepts,
+#' understand good practice and to enable them to conduct common operations
+#' with the data that they are confronted with on a day-to-day basis.
+#'
+#'  @section Package contents:
+#'  An Introduction to R Training Manual
+#'  Sample datasets from government sources
+#'  Mock datasets created in the course of the training
+#'
+#'  @docType package
+#'  @name IntroR
+NULL
